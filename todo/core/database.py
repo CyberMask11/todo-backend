38ed2c308c330be5f3fa2e6141391a14ad215b54
CyberMask11 <johndray977@gmail.com>
2026-01-02 02:21:46 +0200

    Update database.py

--- todo/core/database.py
+++ todo/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://graymask:password@localhost:5432/todo'
+DATABASE_URL = 'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}'
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
